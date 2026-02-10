3b56910b2b509ee03e5b7023fc14648d9e22a5e7
Marion Jhon Agapay <marionagapay2001@gmail.com>
2026-02-10 19:09:44 +0800

    feat: navbar with theme mode toggle

---
+++ src/components/navbar.jsx
@@ -0,0 +1,24 @@
+import { ClipboardList, Moon, Sun } from "lucide-react";
+
+const Navbar = ({ isLight, setIsLight }) => {
+  return (
+    <nav className="border-b border-slate-300 bg-[#EFE9E3] dark:border-white/20 dark:bg-[#000000]">
+      <div className="container mx-auto flex h-16 items-center px-4 justify-between">
+        <div className="flex items-center gap-2 text-lg font-semibold dark:text-white">
+          <ClipboardList /> TO-DO LIST
+        </div>
+        <div>
+          <button onClick={() => setIsLight((prev) => !prev)}>
+            {isLight ? (
+              <Sun className="w-6 h-6 text-amber-500" />
+            ) : (
+              <Moon className="w-6 h-6 text-white" />
+            )}
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
