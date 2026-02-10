56027c6e2f33fcafaa688ce99a7ae2931da89294
Marion Jhon Agapay <marionagapay2001@gmail.com>
2026-02-10 19:26:13 +0800

    feat: hover button

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,11 +4,14 @@ const Navbar = ({ isLight, setIsLight }) => {
   return (
     <nav className="border-b border-slate-300 bg-[#EFE9E3] dark:border-white/20 dark:bg-[#000000]">
       <div className="container mx-auto flex h-16 items-center px-4 justify-between">
-        <div className="flex items-center gap-2 text-lg font-semibold dark:text-white">
+        <div className="flex items-center gap-2 text-base font-semibold dark:text-white">
           <ClipboardList /> TO-DO LIST
         </div>
         <div>
-          <button onClick={() => setIsLight((prev) => !prev)}>
+          <button
+            onClick={() => setIsLight((prev) => !prev)}
+            className="p-2 rounded-full hover:bg-black/10 dark:hover:bg-white/10 transition-colors"
+          >
             {isLight ? (
               <Sun className="w-6 h-6 text-amber-500" />
             ) : (
